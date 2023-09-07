bf3f1a7604200b91d0e6faf033cfbf1b09b752de
Jalen Murray <jalenmmurray@gmail.com>
2023-09-06 22:15:25 -0400

    Changed div it loads in

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GridProvider } from './context/grid';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('demo'));
 root.render(
   <React.StrictMode>
     <GridProvider>
